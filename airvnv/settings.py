5d2842b5371c73d3b11fc9fa7170d6f088790557
maurogch <maurogch@gmail.com>
2019-10-24 03:52:18 -0300

    Set app to site, then configure a simple route, test a hello world controller (view)

--- airvnv/settings.py
+++ airvnv/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'renting.apps.RentingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 USE_I18N = True
 
