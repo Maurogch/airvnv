dbc4f3a328619bd16593051b2e01b0b94508fd21
Mauro Guerreiro <maurogch@gmail.com>
2019-10-24 20:45:23 -0300

    Add templates for cities and cities details, make view controller call template instead of a string

--- renting/urls.py
+++ renting/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 
+app_name = 'renting'
 urlpatterns = [
     path('', views.index, name='index'),
     path('city/<int:city_id>', views.city, name='city'),
+    path('city/', views.citys, name='city'),
+    path('registerProperty/', views.register_property, name='registerProperty'),
 ]
--- renting/views.py
+++ renting/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.http import Http404
 from .models import *
 
 
@@ -7,6 +8,28 @@ def index(request):
     return HttpResponse("Hello world, from default view index")
 
 
-def city(request, city_id):
-    response = "You are looking at city: {0} id: {1}".format(City.objects.get(pk=city_id), city_id)
+def city1(request, city_id):
+    try:
+        response = "You are looking at city: {0} id: {1}".format(City.objects.get(pk=city_id), city_id)
+    except City.DoesNotExist:
+        raise Http404("La ciudad no existe")
     return HttpResponse(response)
+
+
+def city(request, city_id):
+    city = get_object_or_404(City, pk=city_id)
+    return render(request, 'renting/detail.html', {'city': city})
+
+
+def citys(request):
+    cities = City.objects.all()
+    context = {'cities': cities}
+    return render(request, 'renting/index.html', context)
+
+
+def register_property(request):
+    cities = City.objects.all()
+    return render(request, 'renting/register-property.html', {'cities': cities})
+
+
+# def saveProperty(request):
