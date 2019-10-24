0ebfdce06e1ba9b67700cac52bc052946e99b514
maurogch <maurogch@gmail.com>
2019-10-24 17:34:29 -0300

    Test routing with parameters (city/id)

--- renting/urls.py
+++ renting/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('city/<int:city_id>', views.city, name='city'),
 ]
--- renting/views.py
+++ renting/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
 def index(request):
     return HttpResponse("Hello world, from default view index")
+
+
+def city(request, city_id):
+    response = "You are looking at city: {0} id: {1}".format(City.objects.get(pk=city_id), city_id)
+    return HttpResponse(response)
