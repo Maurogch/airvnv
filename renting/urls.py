2ff48ba990e54ddc3972aade2386ab86a8d38d7d
Mauro Guerreiro <maurogch@gmail.com>
2019-11-15 05:21:45 -0300

    Reworked templates to use blocks, added about and contact

--- renting/urls.py
+++ renting/urls.py
@@ -6,7 +6,7 @@ app_name = 'renting'
 urlpatterns = [
     path('', views.index, name='index'),
     path('property/<int:property_id>', views.single_property, name='single_property'),
-    path('city/<int:city_id>', views.city, name='city'),
-    path('city/', views.citys, name='city')
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact')
 
 ]
